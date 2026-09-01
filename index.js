8c8357548924a934e2fd6a319fe05a1df2b0d5a3
igor <igorbernardorocha@gmail.com>
2026-09-01 17:31:25 -0300

    adicionar materias concluida

--- index.js
+++ index.js
@@ -1,53 +1,121 @@
-console.log ("=========================");
-console.log ("DIARIO DE ESTUDOS"); 
-console.log ("========================="); 
-console.log ("1 - Adicionar materias"); 
-console.log ("2 - Listar materias"); 
-console.log ("3 - Pesquisar materias"); 
-console.log ("4 - Calcular o total de horas"); 
-console.log ("5 - Sair\n"); 
+function menu() {
+            console.log ("=========================");
+            console.log ("DIARIO DE ESTUDOS"); 
+            console.log ("========================="); 
+            console.log ("1 - Adicionar materias"); 
+            console.log ("2 - Listar materias"); 
+            console.log ("3 - Pesquisar materias"); 
+            console.log ("4 - Calcular o total de horas"); 
+            console.log ("5 - Sair\n"); 
 
-const readline = require('node:readline'); //funcao da entrada e saida de dados e cria uma vez, utilizadno o rl.question consegue digitar de boa
+            const readline = require('node:readline'); //funcao da entrada e saida de dados e cria uma vez, utilizadno o rl.question consegue digitar de boa
 
-const rl = readline.createInterface({
-  input: process.stdin, //input é a entrada, e process.stdin biblioteca do teclado 
-  output: process.stdout, //output é a saida, process.stdout biblioteca de saida do terminak
-});
-
-rl.question(`Escolha uma opcao: 
-`, opcao => {
-  
-    switch (opcao) {
-        case "1":
-            
-            rl.question("Materia: ", materia => {
-                
-                console.log("teste");
+            const rl = readline.createInterface({
+            input: process.stdin, //input é a entrada, e process.stdin biblioteca do teclado 
+            output: process.stdout, //output é a saida, process.stdout biblioteca de saida do terminak
             });
 
-            break;
-    
-        case "2":
-
-            break;
-
-        case "3":
-
-            break;
+            rl.question(`Escolha uma opcao: `, opcao => {
             
-        case "4":
+                switch (opcao) {
+                    case "1":
+                        console.clear();
 
-            break;
+                        rl.question("Materia: ", materia => {
+                            
+                            rl.question("Horas estudadas (em h): ", horas_estudadas => {
 
-        case "5":
+                                const Vhoras_estudadas = horas_estudadas;
 
-            rl.close();
-            
-            break
-        default:
-            break;
-    }
+                                    console.log("-------------------------")
+                                    console.log ("Qual a dificuldade:");                    
+                                    console.log ("Escolha: 1 - facil");
+                                    console.log ("Escolha: 2 - medio");
+                                    console.log ("Escolha: 3 - dificil");
 
-});
+                                        rl.question("=> ", dificuldade => {
+
+                                            switch (dificuldade) {
+                                                case "1":
+                        
+                                                    const facil = 0;
+                                                    dificuldade = facil;
+                                                    console.log ("-------------------------");
+
+                                                    break;
+                                            
+                                                case "2":
+
+                                                    const medio = 0;
+
+                                                    dificuldade = medio;
+                                                    console.log ("-------------------------");
+
+                                                    break;
+                                                
+                                                case "3":
+
+                                                    const dificil = 0;
+                                                    dificuldade = dificil;
+                                                    console.log ("-------------------------");
+
+                                                    break;
+
+                                            }      
+
+                                                console.log("Observacao");
+                                                console.log("1 - Sim");
+                                                console.log("2 - Nao");
+
+                                                rl.question("=> ", obs =>{
+
+                                                    switch (obs) {
+                                                        case "1":
+                                                            rl.question("Digite: ", escrita => {
+
+                                                                rl.question("Digite aqui:");
+                                                                console.clear();
+                                                                console.log ("-------------------------");
+                                                                console.log("Observação e materia armazenada com sucesso!")
+                                                                console.log ("-------------------------");
+                                                                menu();
+                                                                
+                                                            })
+                                                            break;
+                                                        
+                                                        case "2": 
+                                                            menu();
+                                                    }
+                                                });
+
+                                        });         
+                            })
+                        });
 
 
+                        break;
+                
+                    case "2":
+
+                        break;
+
+                    case "3":
+
+                        break;
+                        
+                    case "4":
+
+                        break;
+
+                    case "5":
+
+                        rl.close();
+                        
+                        break
+                    default:
+                        break;
+                }
+
+            });
+        }
+menu ();
